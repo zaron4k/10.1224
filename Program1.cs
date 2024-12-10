10f382e9cb432bf6da5540895e952406fb56807f
zaron4k <76218026+zaron4k@users.noreply.github.com>
2024-12-10 14:23:40 +0200

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{/// <summary>
+/// Tiraspol_2024
+/// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            //Array.Sort(people);
+            //foreach (var el in people)
+            //    Console.WriteLine(el);
+            //Array.Reverse(people);
+            //foreach (var el in people)
+            //    Console.WriteLine(el);
+            //Console.ReadLine();
+
+
+            //подсчитать колличество числа со значением 5 в одномерном массиве
+            //int[] omas = { 5, 7, 1, 9, 5, 3, 6, 5, 4 };
+            ////int count = 0;
+            ////foreach (var el in omas)
+            ////{
+            ////    if (el == 5)
+            ////    {
+            ////        count++;
+            ////    }
+            ////    Console.WriteLine(count);
+            ////}
+            ////Console.ReadKey();
+
+            //int count = omas.Count(x => x == 5);
+            //Console.WriteLine(count);
+            //Console.ReadLine();
+
+
+            //удалить все четные элементы из одномерного массива
+            int[] omas = { 5, 7, 1, 9, 5, 3, 5};
+            omas = omas.Where(x => x % 2 != 0) . ToArray();
+            Console.WriteLine(omas);
+            Console.Read();
+        }
+    }
+}
