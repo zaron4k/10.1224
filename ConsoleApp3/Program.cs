fa47bad5b1e75a36fb49cf057b18349dbb26ac32
zaron4k <76218026+zaron4k@users.noreply.github.com>
2024-12-10 13:38:26 +0200

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Sam", "Bob", "Kate", "Tom", "Alice" };
+            Array.Sort(people);
+            foreach (var el in people)
+                Console.WriteLine(el);
+            Array.Reverse(people);
+            foreach (var el in people)
+                Console.WriteLine(el);
+            Console.ReadLine();
+        }
+    }
+}
